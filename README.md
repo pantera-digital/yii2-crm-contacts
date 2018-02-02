c842ddf29193dd1e95ab82a7d275c18874c58e10
web-mayak <web-mayak@mail.ru>
2018-02-02 21:57:05 +1000

    initial commit with contributors

---
+++ README.md
@@ -0,0 +1,1 @@
+# Yii2 CRM contacts module
---
+++ src/Module.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace pantera\crm\contacts;
+
+class Module extends \yii\base\Module
+{   
+    public function init()
+    {
+        parent::init();
+    }
+}
