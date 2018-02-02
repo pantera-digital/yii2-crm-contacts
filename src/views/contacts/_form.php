4dfa6145666b90c914be0dc6d01a22a752a317e5
web-mayak <web-mayak@mail.ru>
2018-02-03 00:52:35 +1000

    + на поле телефона контакта добавить маску

--- src/views/contacts/_form.php
+++ src/views/contacts/_form.php
@@ -17,7 +17,9 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'middle_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'phone')->widget(\yii\widgets\MaskedInput::className(), [
+        'mask' => '+7 (999) 999-99-99',
+    ]) ?>
 
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
