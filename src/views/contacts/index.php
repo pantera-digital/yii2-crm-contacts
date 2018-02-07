13922b2e2ee7b12fdfa850fba7c54e8e50b8f700
web-mayak <web-mayak@mail.ru>
2018-02-07 21:56:10 +1000

    fix

--- src/views/contacts/index.php
+++ src/views/contacts/index.php
@@ -60,7 +60,7 @@ CrudAsset::register($this);
                 'type' => 'default', 
                 'heading' => '<i class="glyphicon glyphicon-list"></i> Contacts listing',
                 //'before'=>'<em>* Resize table columns just like a spreadsheet by dragging the column edges.</em>',
-                'after'=>BulkButtonWidget::widget([
+                'footer'=>BulkButtonWidget::widget([
                             'buttons'=>Html::a('<i class="mdi mdi-delete mdi-inverse"></i>&nbsp; Delete All',
                                 ["bulkdelete"] ,
                                 [
