afa47151f5fce467d6984bb9c3c88a2b4be88603
Владимир Курдюков <numkms@gmail.com>
2018-02-16 15:25:22 +1000

    codeception exception -> yii base config exception closes #1

--- src/Module.php
+++ src/Module.php
@@ -2,7 +2,7 @@
 
 namespace pantera\crm\contacts;
 
-use Codeception\Exception\ConfigurationException;
+use yii\base\InvalidConfigException;
 use yii\db\ActiveRecord;
 
 class Module extends \yii\base\Module
@@ -33,7 +33,7 @@ class Module extends \yii\base\Module
     {
         parent::init();
         if(empty($this->userModel)) {
-            throw new ConfigurationException('Пожалуйста, укажите модель пользователя в настройках модуля. Деректива userModel.');
+            throw new InvalidConfigException('Пожалуйста, укажите модель пользователя в настройках модуля. Деректива userModel.');
         }
     }
 }
