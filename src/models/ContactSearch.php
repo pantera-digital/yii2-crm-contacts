abcded249f4050cbdbfc9daa90d963ee063aaf8c
Alex Kostin <web-mayak@mail.ru>
2018-03-03 00:05:40 +1000

    + сортировка контактов по умолчанию id DESC

--- src/models/ContactSearch.php
+++ src/models/ContactSearch.php
@@ -63,6 +63,8 @@ class ContactSearch extends Contact
     {
         $query = Contact::find();
 
+        $query->orderBy('id DESC');
+
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
