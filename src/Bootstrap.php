c67ffa0dd80eeb4fec700261527260e470108176
Alex Kostin <web-mayak@mail.ru>
2018-03-03 00:20:57 +1000

    избавились от /contacts/contacts/index в пользу /contacts

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,6 +4,7 @@ namespace pantera\crm\contacts;
 
 use yii\base\Application;
 use yii\web\View;
+use Yii;
 
 class Bootstrap implements \yii\base\BootstrapInterface
 {
@@ -14,5 +15,14 @@ class Bootstrap implements \yii\base\BootstrapInterface
             	\pantera\crm\contacts\assets\CrmContactsAsset::register($e->sender);
             });
         });
+
+        $configUrlRule = [
+            'class' => 'yii\web\GroupUrlRule',
+            'rules' => [
+                'contacts' => 'contacts/contacts/index',
+            ]
+        ];
+        $rule = Yii::createObject($configUrlRule);
+        $app->urlManager->addRules([$rule], false);
     }
 }
