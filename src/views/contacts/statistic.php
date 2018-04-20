ad0633543847a4c1977ab79181e1f4ed861c861b
Владимир Курдюков <numkms@gmail.com>
2018-04-21 00:55:18 +1000

    link to filter

--- src/views/contacts/statistic.php
+++ src/views/contacts/statistic.php
@@ -4,6 +4,7 @@
  * @var \yii\web\View $this
  */
 use dosamigos\chartjs\ChartJs;
+use pantera\crm\contacts\models\Param;
 
 $this->title = "Статистика контактов";
 $this->params['breadcrumbs'][] = ['url' => ['/contacts/contacts/index'], 'label' => 'Контакты'];
@@ -11,11 +12,11 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="row">
-    <?php foreach ($widgetsData as $key => $widgetData):?>
+    <?php foreach ($widgetsData as $groupId => $widgetData):?>
         <div class="col-md-6">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <?=\pantera\crm\contacts\models\ParamGroup::findOne($key)->name;?>
+                    <?=\pantera\crm\contacts\models\ParamGroup::findOne($groupId)->name;?>
                 </div>
                 <div class="panel-body">
                     <?= ChartJs::widget([
@@ -33,7 +34,9 @@ $this->params['breadcrumbs'][] = $this->title;
                     <?php foreach ($widgetData['labels'] as $key => $label):?>
                         <tr style="background:<?=$widgetData['datasets'][0]['backgroundColor'][$key]?>;opacity: 0.8;">
                             <th style="opacity: 1;">
-                                <?=$label?>
+                                <?=\yii\helpers\Html::a($label,['/contacts/contacts/index','ContactSearch' => [
+                                     'tags' => [Param::findOne(['group_id' => $groupId, 'name' => $label])->id],
+                                ]])?>
                             </th>
                             <td style="opacity: 1;">
                                 <?=$widgetData['datasets'][0]['data'][$key]?>
